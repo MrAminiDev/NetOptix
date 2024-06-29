cc74755def5029852698917dcc83f922280f59bd
Pouria Amini <168713477+MrAminiDev@users.noreply.github.com>
2024-06-29 13:47:24 +0330

    Update DPing.sh

--- scripts/DPing.sh
+++ scripts/DPing.sh
@@ -8,9 +8,12 @@
 #
 # my Github: https://github.com/MrAminiDev/
 
+sudo apt-get update
+sudo apt-get install -y iptables-persistent
+
 sudo iptables -A INPUT -p icmp --icmp-type echo-request -j DROP
 
-sudo sh -c "iptables-save > /etc/iptables/rules.v4"
+sudo netfilter-persistent save
 
 echo "Ping requests have been blocked."
 
