4fcc14be1e7828ef19f5e4df31f785f2a45fc2b0
Pouria Amini <168713477+MrAminiDev@users.noreply.github.com>
2024-07-13 14:12:30 +0330

    Update block-isp.sh

--- scripts/isp-blocker/block-isp.sh
+++ scripts/isp-blocker/block-isp.sh
@@ -8,6 +8,7 @@
 #
 # my Github: https://github.com/MrAminiDev/
 
+
 install_prerequisites() {
     if ! command -v iptables &> /dev/null; then
         echo "iptables not found. Installing..."
@@ -62,58 +63,72 @@ unblock_all() {
 
 show_isp_menu() {
     clear
-    echo "Choose an ISP:"
-    echo "1) Irancell"
-    echo "2) Hamrah-Aval (MCI)"
-    echo "3) Rightel"
-    read -p "Enter your choice: " isp_choice
+    while true; do
+        echo "Choose an ISP:"
+        echo "1) Irancell"
+        echo "2) Hamrah-Aval (MCI)"
+        echo "3) Rightel"
+        echo "4) Back to main menu"
+        read -p "Enter your choice: " isp_choice
 
-    case $isp_choice in
-        1)
-            isp_url="https://raw.githubusercontent.com/MrAminiDev/NetOptix/main/scripts/isp-blocker/isp/irancell.ipv4"
-            isp_name="Irancell"
-            ;;
-        2)
-            isp_url="https://raw.githubusercontent.com/MrAminiDev/NetOptix/main/scripts/isp-blocker/isp/mci.ipv4"
-            isp_name="MCI"
-            ;;
-        3)
-            isp_url="https://raw.githubusercontent.com/MrAminiDev/NetOptix/main/scripts/isp-blocker/isp/rightel.ipv4"
-            isp_name="Rightel"
-            ;;
-        *)
-            echo "Invalid choice."
-            exit 1
-            ;;
-    esac
+        case $isp_choice in
+            1)
+                isp_url="https://raw.githubusercontent.com/MrAminiDev/NetOptix/main/scripts/isp-blocker/isp/irancell.ipv4"
+                isp_name="Irancell"
+                break
+                ;;
+            2)
+                isp_url="https://raw.githubusercontent.com/MrAminiDev/NetOptix/main/scripts/isp-blocker/isp/mci.ipv4"
+                isp_name="MCI"
+                break
+                ;;
+            3)
+                isp_url="https://raw.githubusercontent.com/MrAminiDev/NetOptix/main/scripts/isp-blocker/isp/rightel.ipv4"
+                isp_name="Rightel"
+                break
+                ;;
+            4)
+                return
+                ;;
+            *)
+                echo "Invalid choice. Please try again."
+                ;;
+        esac
+    done
 }
 
 main_menu() {
     clear
-    echo "Please choose one of the options below"
-    echo "1) Block ISP"
-    echo "2) Unblock ISP"
-    echo "3) Unblock all ISP"
-    read -p "Enter your choice: " main_choice
+    while true; do
+		echo "Please choose one of the options below"
+        echo "1) Block ISP"
+        echo "2) Unblock ISP"
+        echo "3) Unblock all ISP"
+        echo "4) Exit"
+        read -p "Enter your choice: " main_choice
 
-    case $main_choice in
-        1)
-            show_isp_menu
-            block_ips $isp_url $isp_name
-            ;;
-        2)
-            show_isp_menu
-            unblock_ips $isp_url $isp_name
-            ;;
-        3)
-            unblock_all
-            ;;
-        *)
-            echo "Invalid choice."
-            exit 1
-            ;;
-    esac
-    clear
+        case $main_choice in
+            1)
+                show_isp_menu
+                block_ips $isp_url $isp_name
+                ;;
+            2)
+                show_isp_menu
+                unblock_ips $isp_url $isp_name
+                ;;
+            3)
+                unblock_all
+                ;;
+            4)
+                clear
+                exit 0
+                ;;
+            *)
+                echo "Invalid choice. Please try again."
+                ;;
+        esac
+        clear
+    done
 }
 
 install_prerequisites
