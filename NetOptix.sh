8140405d20497e9ea67d930ebce1d8e0dd4eaa14
Pouria Amini <168713477+MrAminiDev@users.noreply.github.com>
2024-07-17 17:37:03 +0330

    Update NetOptix.sh

--- NetOptix.sh
+++ NetOptix.sh
@@ -16,7 +16,7 @@ while true; do
     echo -e "     ${RED}Open issues at https://github.com/MrAminiDev/NetOptix ${NC}"
     echo -e "${BLUE}+======================================================================+${NC}"
     echo -e "${BLUE}+======================================================================+${NC}"
-    echo -e "                         ${RED}NetOptix V1.8.2  ${NC}"
+    echo -e "                         ${RED}NetOptix V1.8.3  ${NC}"
     echo -e "${BLUE}+======================================================================+${NC}"
     echo "1- install Hybla"
     echo "2- install BBR"
