6751ef57a6025b19c4e3ee6877bd2b5677f09375
Pouria Amini <168713477+MrAminiDev@users.noreply.github.com>
2025-01-09 14:48:58 +0330

    Update NetOptix.sh

--- NetOptix.sh
+++ NetOptix.sh
@@ -211,7 +211,7 @@ while true; do
 	echo -e	"${RED}##   ### ##          ##    ##     ## ##           ##     ##   ##   ##  ${NC}"
 	echo -e	"${RED}##    ## ########    ##     #######  ##           ##    #### ##     ## ${NC}"
 	echo -e "${CYAN}+======================================================================+${NC}"
-    echo -e "|  Telegram Channel : ${MAGENTA}@AminiDev ${NC}|  Version : ${GREEN} 4.0.1${NC} "
+    echo -e "|  Telegram Channel : ${MAGENTA}@AminiDev ${NC}|  Version : ${GREEN} 4.0.2${NC} "
 	echo -e "${CYAN}+======================================================================+${NC}"
     echo -e "${CYAN}== Main Menu ==${NC}"
     echo -e "1) Network Optimization"
